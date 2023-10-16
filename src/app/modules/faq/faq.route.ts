93e6bbc99b962b6c9cdd77cf4d3e718b74784373
Programmer-Mehadi <mehadi.developer@gmail.com>
2023-10-16 15:02:38 +0600

    faq done

---
+++ src/app/modules/faq/faq.route.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import { FaqController } from './faq.controller'
+import auth from '../../middlewares/auth'
+import { ADMIN } from '../../../shared/user'
+
+const router = express.Router()
+
+router.post('/', auth(ADMIN), FaqController.createFaq)
+router.get('/get-all', auth(ADMIN), FaqController.getAll)
+router.put('/status-change/:id', auth(ADMIN), FaqController.statusChange)
+router.delete('/delete/:id', auth(ADMIN), FaqController.deleteFaq)
+router.get('/get/:id', auth(ADMIN), FaqController.getSingleFaq)
+router.put('/update/:id', auth(ADMIN), FaqController.updateFaq)
+export const FaqRoutes = router
